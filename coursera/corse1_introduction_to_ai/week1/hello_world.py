8db05ef1f9b15737c456a7e78c69e662ceb8429c
Neeraj Komuravalli <komunee2710@gmail.com>
2021-05-11 14:09:15 +0530

    Adding hello world of keras

---
+++ coursera/corse1_introduction_to_ai/week1/hello_world.py
@@ -0,0 +1,14 @@
+import keras
+import numpy as np
+
+
+if __name__ == "__main__":
+    model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+    model.compile(optimizer="sgd", loss="mean_squared_error")
+
+    xs = np.array([-1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0], dtype=float)
+    xy = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0, 9.0], dtype=float)
+
+    model.fit(xs, xy, epochs=500)
+
+    print(model.predict([10.0]))
